760cb9601f58717f08a86c55493c394bd30f259e
XPoet <i@xpoet.cn>
2020-08-18 18:15:33 +0800

    init image management page

--- src/views/Management.vue
+++ src/views/Management.vue
@@ -1,54 +1,46 @@
 <template>
   <div>
-    <el-button @click="getUserInfo"
-    >getUserInfo
-    </el-button>
+   <h1>coming soon...</h1>
   </div>
 </template>
 
 <script>
-
   import Axios from 'axios'
+  import {picx_key} from "../utils/localStorage";
 
   export default {
     name: "Management",
 
     data() {
-      return {}
+      return {
+        userConfigInfo: null
+      }
     },
 
     mounted() {
-
-
+      this.getUserConfigInfo()
     },
 
     methods: {
 
-      getUserInfo() {
-
-        let config = localStorage.getItem('PICX')
+      getUserConfigInfo() {
+        let config = localStorage.getItem(picx_key)
         if (config) {
-          config = JSON.parse(config)
+          this.userConfigInfo = JSON.parse(config)
         }
-        Axios.get(
-          'https://api.github.com/user',
-          {
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": `token ${config.token}`
-            }
-          }
-        ).then(res => {
-          console.log('res', res);
-        })
       },
 
-      getRepos() {
+      getReposInfo() {
+        if (this.userConfigInfo) {
 
-        Axios.get('https://api.github.com/repos/XPoet/xpoet-image-hosting/contents')
-          .then(res => {
-            console.log('res', res);
-          })
+          Axios.get(`https://api.github.com/repos/${this.userConfigInfo?.username}/${this.userConfigInfo?.selectedRepos}/contents`)
+            .then(res => {
+              console.log('res', res);
+            })
+
+        }
+
+
 
       }
     }
