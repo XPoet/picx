ae912502bdda033beb8dc54702189a6a3ff6765d
XPoet <i@xpoet.cn>
2020-08-22 23:03:41 +0800

    style: import global css style file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './style.scss'
 
 // 导入 ElementUI
 import ElementUI from 'element-ui';
