5eeecf5f12af6659a7f000a042fc9a0ddb95f2cb
XPoet <i@xpoet.cn>
2020-08-28 15:27:18 +0800

    pref: config page add root dir option

--- src/common/utils/uploadUrlHandle.js
+++ src/common/utils/uploadUrlHandle.js
@@ -2,7 +2,7 @@ const uploadUrlHandle = (config, filename) => {
 
   let path = config.selectedDir + '/'
 
-  if (config.dirMode === 'nonuseDir') {
+  if (config.dirMode === 'rootDir') {
     path = ''
   }
   return `/repos/${config.owner}/${config.selectedRepos}/contents/${path}${filename}`
