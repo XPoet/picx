af74d5199b7172537bad5d9fb6fb0ab19fa478f4
XPoet <i@xpoet.cn>
2021-04-25 19:05:32 +0800

    refactor(element-ui): add new file to import element-plus component

--- src/common/utils/importUIFramework.ts
+++ src/common/utils/importUIFramework.ts
@@ -1,8 +1,49 @@
 import { App } from 'vue'
-import { ElIcon, ElLoading, ElCard, ElButton } from 'element-plus'
+import {
+  ElIcon,
+  ElLoading,
+  ElButton,
+  ElSelect,
+  ElTooltip,
+  ElForm,
+  ElInput,
+  ElBadge,
+  ElRadioGroup,
+  ElRadio,
+  ElStep,
+  ElSteps,
+  ElCheckbox,
+  ElTag,
+  ElLink,
+  ElDropdown,
+  ElDropdownItem,
+  ElDropdownMenu,
+  ElOption,
+  ElFormItem
+} from 'element-plus'
 
-export default function importUiFramework(app: App) {
-  // 按需载入 Element Plus 组件
-  app.use(ElButton).use(ElCard).use(ElLoading).use(ElIcon)
+// 按需载入 Element Plus 组件
+export default function importUIFramework(app: App) {
+  app
+    .use(ElButton)
+    .use(ElSelect)
+    .use(ElTooltip)
+    .use(ElForm)
+    .use(ElInput)
+    .use(ElBadge)
+    .use(ElIcon)
+    .use(ElRadioGroup)
+    .use(ElRadio)
+    .use(ElLoading)
+    .use(ElStep)
+    .use(ElSteps)
+    .use(ElCheckbox)
+    .use(ElTag)
+    .use(ElLink)
+    .use(ElDropdown)
+    .use(ElDropdownItem)
+    .use(ElDropdownMenu)
+    .use(ElOption)
+    .use(ElFormItem)
   return app
 }
--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,10 @@
 import { createApp } from 'vue'
+import importUIFramework from '@/common/utils/importUIFramework'
+import router from '@/router/index'
+import store from '@/store/index'
 import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
-import {
-  ElButton,
-  ElSelect,
-  ElTooltip,
-  ElForm,
-  ElInput,
-  ElBadge,
-  ElIcon,
-  ElRadioGroup,
-  ElRadio,
-  ElLoading,
-  ElLink,
-  ElStep,
-  ElSteps,
-  ElCheckbox,
-  ElTag,
-  ElDropdown,
-  ElDropdownItem,
-  ElDropdownMenu,
-  ElOption,
-  ElFormItem
-} from 'element-plus'
 
 import './style.styl'
 
-createApp(App)
-  .use(router)
-  .use(store)
-  // 按需载入 Element Plus
-  .use(ElButton)
-  .use(ElSelect)
-  .use(ElTooltip)
-  .use(ElForm)
-  .use(ElInput)
-  .use(ElBadge)
-  .use(ElIcon)
-  .use(ElRadioGroup)
-  .use(ElRadio)
-  .use(ElLoading)
-  .use(ElStep)
-  .use(ElSteps)
-  .use(ElCheckbox)
-  .use(ElTag)
-  .use(ElLink)
-  .use(ElDropdown)
-  .use(ElDropdownItem)
-  .use(ElDropdownMenu)
-  .use(ElOption)
-  .use(ElFormItem)
-  .mount('#app')
+const app = createApp(App)
+importUIFramework(app).use(router).use(store).mount('#app')
