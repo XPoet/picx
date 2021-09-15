7e3e4f8735e379590e4021e0ea9becf4e7c100b2
XPoet <i@xpoet.cn>
2021-09-15 23:26:04 +0800

    feat: add settings page

---
+++ src/views/settings/settings.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page-container settings-page-container">
+    <h1>我的设置</h1>
+    <h2>是否默认给图片加上哈希码</h2>
+    <h2>是否默认开启 Markdown 格式的外链</h2>
+    <h2>是否自动更新图片存储的目录</h2>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped lang="stylus">
+@import "settings.styl"
+</style>
