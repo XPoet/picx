c54d8c9d888335078abb54378c775f9d27d11dd5
huang_dada <609004102@qq.com>
2021-12-05 20:00:12 +0800

    feat(module:style,main,theme,setting,style-import,user-config-info): add dark theme switching

--- src/views/settings/settings.vue
+++ src/views/settings/settings.vue
@@ -16,6 +16,43 @@
           active-text="上传后默认开启 Markdown 格式的外链"
         ></el-switch>
       </li>
+      <li class="setting-item">
+        <el-form ref="form" label-width="150px">
+          <el-form-item label="主题模式:">
+            <el-select
+              v-model="userConfigInfo.personalSetting.themeMode"
+              placeholder="主题模式"
+              @change="saveConfigInfo"
+            >
+              <el-option label="自适应" value="auto"></el-option>
+              <el-option label="暗黑主题" value="dark"></el-option>
+              <el-option label="白亮主题" value="light"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item
+            label="自定义白亮主题时间:"
+            v-if="userConfigInfo.personalSetting.themeMode === 'auto'"
+          >
+            <el-time-select
+              v-model="userConfigInfo.personalSetting.autoLightThemeDate[0]"
+              placeholder=""
+              start="00:00"
+              step="00:15"
+              end="23:59"
+              @change="saveConfigInfo"
+            ></el-time-select>
+            <span class="time-middle-space">--</span>
+            <el-time-select
+              v-model="userConfigInfo.personalSetting.autoLightThemeDate[1]"
+              placeholder="Start time"
+              :start="userConfigInfo.personalSetting.autoLightThemeDate[0]"
+              step="00:15"
+              end="23:59"
+              @change="saveConfigInfo"
+            ></el-time-select>
+          </el-form-item>
+        </el-form>
+      </li>
     </ul>
   </div>
 </template>
@@ -28,6 +65,15 @@ const userConfigInfo = computed(() => store.getters.getUserConfigInfo).value
 
 const switchChange = () => {
   store.dispatch('USER_CONFIG_INFO_PERSIST')
+}
+const saveConfigInfo = () => {
+  console.log('userConfigInfo--', userConfigInfo)
+  store.dispatch('SET_USER_CONFIG_INFO', {
+    ...userConfigInfo,
+    personalSetting: {
+      ...userConfigInfo.personalSetting
+    }
+  })
 }
 </script>
 
