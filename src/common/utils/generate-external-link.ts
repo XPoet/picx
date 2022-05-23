09647db34b7e21b20afa7a31995334ddfc7b8296
RubyXun <65531138+RubyXun@users.noreply.github.com>
2022-05-23 15:13:18 +0800

    Replace the CDN acceleration service with a Statically CDN
    原有Jsdelivr CDN可能暂时被墙,现替换为Statically CDN 正常提供CDN外链服务

--- src/common/utils/generate-external-link.ts
+++ src/common/utils/generate-external-link.ts
@@ -8,7 +8,7 @@ const generateExternalLink = (
   config: UserConfigInfoModel
   // eslint-disable-next-line consistent-return
 ): string => {
-  const cdnLink: string = `https://cdn.jsdelivr.net/gh/${config.owner}/${config.selectedRepos}@${config.selectedBranch}/${content.path}`
+  const cdnLink: string = `https://cdn.staticaly.com/gh/${config.owner}/${config.selectedRepos}@${config.selectedBranch}/${content.path}`
   const ghLink: string = decodeURI(content.download_url)
 
   // eslint-disable-next-line default-case
