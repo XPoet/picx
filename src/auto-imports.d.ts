8078e4a9f4ccef9fcba6c7c2c9429fd96ccf5cba
XPoet <i@xpoet.cn>
2023-04-17 23:09:28 +0800

    perf: optimize copy image link notification

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -7,4 +7,5 @@ declare global {
   const ElLoading: typeof import('element-plus/es')['ElLoading']
   const ElMessage: typeof import('element-plus/es')['ElMessage']
   const ElMessageBox: typeof import('element-plus/es')['ElMessageBox']
+  const ElNotification: typeof import('element-plus/es')['ElNotification']
 }
