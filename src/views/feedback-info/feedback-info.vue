0a92f2461f9b32a28d436532f5f6efe783bcdd85
XPoet <i@xpoet.cn>
2023-05-30 22:02:21 +0800

    feat(feedback): support i18n

--- src/views/feedback-info/feedback-info.vue
+++ src/views/feedback-info/feedback-info.vue
@@ -1,49 +1,31 @@
 <template>
   <div class="page-container feedback-page-container">
-    <div class="help-info-item description">
-      🏞️ PicX 是一款基于 GitHub API
-      开发的图床工具，提供图片上传托管、生成图片链接和常用图片工具箱服务。
+    <div class="help-info-item description">🏞️ {{ $t('feedback.text_1') }}</div>
+
+    <div class="help-info-item">
+      🌍 {{ $t('feedback.text_2') }} {{ $t('shortcut_key') }}：<code>{{ shortcutKey }}</code>
     </div>
 
     <div class="help-info-item">
-      🌍 建议将本站添加到浏览器收藏夹，方便下次使用。快捷键：<code>{{ shortcutKey }}</code>
-    </div>
-
-    <div class="help-info-item">
-      🦁 作者：
+      🦁 {{ $t('author') }}：
       <el-link type="primary" href="https://xpoet.cn/" target="_blank">@XPoet</el-link>
     </div>
 
     <div class="help-info-item">
-      🌀 仓库：
+      🌀 {{ $t('repo') }}：
       <el-link type="primary" href="https://github.com/XPoet/picx" target="_blank">
         https://github.com/XPoet/picx
       </el-link>
     </div>
 
     <div class="help-info-item">
-      📖 文档：
+      📖 {{ $t('document') }}：
       <el-link type="primary" href="https://picx-docs.xpoet.cn" target="_blank">
         https://picx-docs.xpoet.cn
       </el-link>
     </div>
 
-    <div class="help-info-item">
-      💡 在使用过程中如遇到问题，请仔细阅读文档，或者给作者提&nbsp;
-      <el-link
-        type="primary"
-        style="font-size: 16rem"
-        href="https://github.com/XPoet/picx/issues"
-        target="_blank"
-      >
-        Issue
-      </el-link>
-      &nbsp;。
-    </div>
-
-    <div class="help-info-item" style="margin-bottom: 2rem">
-      🎁 如果 PicX 对你有帮助，欢迎赞赏作者，支持开源。
-    </div>
+    <div class="help-info-item" style="margin-bottom: 2rem">🎁 {{ $t('feedback.text_3') }}</div>
 
     <div class="help-info-item img">
       <el-image
@@ -53,9 +35,7 @@
     </div>
 
     <div class="help-info-item red-text">
-      <strong>
-        ⚠️ 郑重声明：请勿使用 PicX 上传违反你当地法律的图片，所造成的一切后果与作者无关。
-      </strong>
+      <strong> ⚠️ {{ $t('feedback.text_4') }} </strong>
     </div>
   </div>
 </template>
