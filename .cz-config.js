51061516684993bfc30661364f92c539a4946cf9
XPoet <i@xpoet.cn>
2023-12-07 16:42:21 +0800

    build(deps): enhanced commitlint

--- .cz-config.js
+++ .cz-config.js
@@ -1,82 +1,84 @@
 module.exports = {
-  // type 类型（定义之后，可通过上下键选择）
+  // Commit 类型列表
   types: [
-    { value: 'feat', name: 'feat:     新增功能' },
-    { value: 'fix', name: 'fix:      修复 bug' },
-    { value: 'docs', name: 'docs:     文档变更' },
-    { value: 'style', name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）' },
-    { value: 'refactor', name: 'refactor: 代码重构（不包括 bug 修复、功能新增）' },
-    { value: 'perf', name: 'perf:     性能优化' },
-    { value: 'test', name: 'test:     添加、修改测试用例' },
-    { value: 'build', name: 'build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）' },
-    { value: 'ci', name: 'ci:       修改 CI 配置、脚本' },
-    { value: 'chore', name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
-    { value: 'revert', name: 'revert:   回滚 commit' }
-  ],
+    ['feat', '新增功能', '✨'],
+    ['fix', '修复 Bug', '🐛'],
+    ['docs', '文档变更', '📝'],
+    ['style', '不影响代码功能的更改（例如：空格、格式化、补齐分号等）', '🎨'],
+    ['refactor', '代码重构（不包括新增功能、修复 Bug）', '🔨'],
+    ['perf', '提高性能的代码更改', '📈'],
+    ['test', '添加、修改测试用例', '✅'],
+    ['build', '构建流程、外部依赖变更（如升级依赖、修改 Vite 配置等）', '🛠️'],
+    ['ci', '修改 CI 配置、脚本', '🔧'],
+    ['chore', '对构建过程或辅助工具和库的更改（不影响源文件、测试用例）', '📦'],
+    ['revert', '回滚 Commit', '⏪'],
+    ['WIP', '正在进行的工作', '🚧'],
+    ['ui', '修改界面 UI', '💄'],
+    ['release', '发布版本或标签', '🚀']
+  ].map(([value, desc, icon]) => {
+    return {
+      value,
+      name: `${(value + ': ').padEnd(10)}${icon} ${desc}`
+    };
+  }),
 
-  // scope 类型（定义之后，可通过上下键选择）
+  // Scopes 类型列表
   scopes: [
     ['components', '组件相关'],
     ['hooks', 'hook 相关'],
-    ['utils', 'utils 相关'],
-    ['element-ui', '对 element-ui 的调整'],
+    ['utils', 'util 相关'],
+    ['element-plus', '对 element-plus 的调整'],
     ['styles', '样式相关'],
     ['deps', '项目依赖'],
     ['auth', '对 auth 修改'],
-    ['other', '其他修改'],
-    // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
-    ['custom', '以上都不是？我要自定义']
-  ].map(([value, description]) => {
+    ['other', '其他修改']
+  ].map(([value, desc]) => {
     return {
       value,
-      name: `${value.padEnd(30)} (${description})`
-    }
+      name: `${value.padEnd(20)}（${desc}）`
+    };
   }),
 
-  // 是否允许自定义填写 scope，在 scope 选择的时候，会有 empty 和 custom 可以选择。
-  // allowCustomScopes: true,
-
-  // allowTicketNumber: false,
-  // isTicketNumberRequired: false,
-  // ticketNumberPrefix: 'TICKET-',
-  // ticketNumberRegExp: '\\d{1,5}',
-
-
-  // 针对每一个 type 去定义对应的 scopes，例如 fix
   /*
+  // 设置在只有选择了 fix 类型才会出现的 scopes
   scopeOverrides: {
     fix: [
-      { name: 'merge' },
-      { name: 'style' },
-      { name: 'e2eTest' },
-      { name: 'unitTest' }
+      {name: 'merge'},
+      {name: 'style'},
+      {name: 'e2eTest'},
+      {name: 'unitTest'}
     ]
   },
-  */
+ */
 
-  // 交互提示信息
+  allowCustomScopes: true, // 允许自定义 Scope
+  allowEmptyScopes: true, // 允许空 Scope
+  customScopesName: `${'custom'.padEnd(20)}（自定义 Scope）`, // 选择自定义 Scope 时的描述
+  emptyScopesName: `${'empty'.padEnd(20)}（不填 Scope）`, // 选择空 Scope 时的描述
+
   messages: {
-    type: '确保本次提交遵循 Angular 规范！\n选择你要提交的类型：',
-    scope: '\n选择一个 scope（可选）：',
-    // 选择 scope: custom 时会出下面的提示
-    customScope: '请输入自定义的 scope：',
-    subject: '填写简短精炼的变更描述：\n',
-    body:
-      '填写更加详细的变更描述（可选）。使用 "|" 换行：\n',
-    breaking: '列举非兼容性重大的变更（可选）：\n',
-    footer: '列举出所有变更的 ISSUES CLOSED（可选）。 例如: #31, #34：\n',
-    confirmCommit: '确认提交？'
+    type: '请选择 Commit 类型：（上下键选择，按 Enter 键确定）',
+    scope: '\n请选择或输入修改范围 Scope：（上下键选择，按 Enter 键确定）',
+    customScope: '请输入自定义的 Scope：', // 需设置 allowCustomScopes: true
+    subject: '填写简短精炼的变更描述：（必填）\n',
+    body: '填写更加详细的变更描述：（使用 \'|\' 换行。非必填，可按 Enter 键跳过）\n',
+    breaking: '列举非兼容性重大的变更：（非必填，可按 Enter 键跳过）\n',
+    footer: '列举出所有变更的 ISSUES CLOSED：（例如：#31、#34。非必填，可按 Enter 键跳过）\n',
+    confirmCommit: '确认使用以上信息提交？（y/n/e）'
   },
 
-  // 设置只有 type 选择了 feat 或 fix，才询问 breaking message
-  allowBreakingChanges: ['feat', 'fix'],
 
-  // 跳过要询问的步骤
-  // skipQuestions: ['body', 'footer'],
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
 
-  // subject 限制长度
-  subjectLimit: 100,
-  breaklineChar: '|' // 支持 body 和 footer
+  allowBreakingChanges: ['feat', 'fix'], // 设置 type 选择 feat 或 fix，询问 breaking message
+  // skipQuestions: ['body', 'footer'], // 跳过问题
+  skipEmptyScopes: false,
+
+  subjectLimit: 100 // subject 长度限制
+  // breaklineChar: '|', // 设置换行符
   // footerPrefix : 'ISSUES CLOSED:'
-  // askForBreakingChangeFirst : true,
+  // askForBreakingChangeFirst : true, // default is false
 }
