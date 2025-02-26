45f39af9b6c165bf321e6e33b561e19237b59944
XPoet <XPoet@users.noreply.github.com>
2025-02-26 08:37:30 +0000

    Update PicX a1ebbb82a4bdae2c5ff70310d93848f425b4883f

--- pluginWebUpdateNotice/webUpdateNoticeInjectScript.global.744c09e4.js
+++ pluginWebUpdateNotice/webUpdateNoticeInjectScript.global.744c09e4.js
@@ -16,5 +16,5 @@
 
   window.__checkUpdateSetup__({"logVersion":true,"injectFileBase":"./"});
       ;const logFn = function pt(t,s){console.log(`version: %c${t}`,"color: #1677ff"),console.log(`release time: %c${new Date(s).toLocaleString()}`,"color: #1677ff")}
-      ;logFn('60c9c6d', 1714467132944)
+      ;logFn('a1ebbb8', 1740558964875)
     
